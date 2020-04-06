763a6e35085c5236385be0f1c5ab9f85e1cc3385
KrylovDenisK <d.krylovk@gmail.com>
2020-04-06 17:22:02 +0700

    1.1. Состояние нити. [#229175]

---
+++ chapter_001/src/main/java/ru/job4j/cuncurrent/TreadState.java
@@ -0,0 +1,26 @@
+package ru.job4j.cuncurrent;
+
+public class TreadState {
+    public Thread getThread() {
+        return new Thread(
+                () -> { });
+    }
+
+    public static void main(String[] args) {
+        TreadState treadState = new TreadState();
+        Thread first = treadState.getThread();
+        Thread second = treadState.getThread();
+        System.out.println(first.getName() + " " + first.getState() + "   "
+                + second.getName() + " " + second.getState());
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED
+                | second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getName() + " " + first.getState() + "   "
+                    + second.getName() + " " + second.getState());
+        }
+        System.out.println(first.getState() + "  " + second.getState());
+        System.out.println("First and Second threads is completed!");
+
+    }
+}
