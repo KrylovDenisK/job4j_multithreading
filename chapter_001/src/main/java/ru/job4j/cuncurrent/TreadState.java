39d3ff7febce097316a824a7b1b8da08d7576b0c
KrylovDenisK <d.krylovk@gmail.com>
2020-05-16 12:38:02 +0700

    1.1. Состояние нити. изменения

--- chapter_001/src/main/java/ru/job4j/cuncurrent/TreadState.java
+++ chapter_001/src/main/java/ru/job4j/cuncurrent/TreadState.java
@@ -15,7 +15,7 @@ public class TreadState {
         first.start();
         second.start();
         while (first.getState() != Thread.State.TERMINATED
-                | second.getState() != Thread.State.TERMINATED) {
+                || second.getState() != Thread.State.TERMINATED) {
             System.out.println(first.getName() + " " + first.getState() + "   "
                     + second.getName() + " " + second.getState());
         }
